b2e931b62ce43de3c51cfa0be267f00965addc52
Jung-uk-jin <jungukjin32@gmail.com>
2025-01-14 18:25:54 +0900

    0114 자바 수업2

---
+++ j0114/src/j0114/Ja0114_09.java
@@ -0,0 +1,22 @@
+package j0114;
+
+public class Ja0114_09 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int score1=0;
+		int score2=1;
+		
+		int[] score = new int[5];
+		score[0]=0;
+		score[1]=1;
+		score[2]=2;
+		score[3]=3;
+		score[4]=4;
+		
+		for(int i=0 ; i<5 ; i++) {
+			System.out.println(score[i]);
+		}
+	}
+	
+}
